2b6f414df458849184dcc00e26f059d523c67f29
mizaan-sk <mizaandev@gmail.com>
2026-06-08 19:32:27 +0530

    Solved

--- components/SmoothScroll.js
+++ components/SmoothScroll.js
@@ -1,5 +1,6 @@
+'use client'
 import { useEffect } from 'react'
-import Lenis from '@studio-freight/lenis'
+import Lenis from 'lenis';
 export default function SmoothScroll({children}) {
     useEffect( () => {
         window.scrollTo(0,0);
